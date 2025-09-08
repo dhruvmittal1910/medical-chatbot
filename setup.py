ce64f5c41f0632f130ff2fa9805d72ff93ad2a63
Dhruv Mittal <dhruvmittal1910@gmail.com>
2025-09-07 22:38:16 -0400

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='medical-chatbot',
+    author_name='dhruv',
+    version='0.1',
+    author_email='dhruvmittal1910@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
